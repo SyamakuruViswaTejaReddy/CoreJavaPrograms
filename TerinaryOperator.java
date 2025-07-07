fa5ab5f294e5577ba9f213ab379d0413017811d1
SyamakuruViswaTejaReddy <syamakuruviswatejareddy@gmail.com>
2025-07-07 14:06:20 +0530

    Add TerinaryOperator class to demonstrate the use of the ternary operator in Java

---
+++ TerinaryOperator.java
@@ -0,0 +1,7 @@
+public class TerinaryOperator {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+        int max = (a > b) ? a : b; // Ternary operator to find maximum
+        System.out.println("Maximum of a and b is: " + max);
+    }
+}
