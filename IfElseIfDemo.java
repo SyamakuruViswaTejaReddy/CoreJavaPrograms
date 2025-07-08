a0ed4abee92dd3e9cddf213d7e7dcdb9e51f00b7
SyamakuruViswaTejaReddy <syamakuruviswatejareddy@gmail.com>
2025-07-08 10:18:14 +0530

    conditional statements in Java

---
+++ IfElseIfDemo.java
@@ -0,0 +1,12 @@
+public class IfElseIfDemo {
+    public static void main(String[] args) {
+        int x = 10;
+        if (x > 10) {
+            System.out.println("x is greater than 10");
+        } else if (x == 10) {
+            System.out.println("x is equal to 10");
+        } else {
+            System.out.println("x is less than 10");
+        }
+    }
+}
